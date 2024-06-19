9957d6343c8c18aab0240c77d1961321dc406b7f
James Magpantay <james.magpantay11@gmail.com>
2024-06-18 21:16:15 -0400

    account app boot

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,17 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let app: AppComponent;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        HttpClientModule
       ],
       declarations: [
         AppComponent
@@ -14,22 +19,85 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
+  beforeEach(()=> {
+     fixture = TestBed.createComponent(AppComponent);
+     app = fixture.componentInstance;
+  })
+
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+ 
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'cibc-app'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+
     expect(app.title).toEqual('cibc-app');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('cibc-app app is running!');
+  it(`should return sum of balance of all accounts`, () => {
+    const accounts = [
+      {
+        "type": "INVESTMENT",
+        "balance": 565656,
+        "id": "12345678918"
+      },
+      {
+        "type": "CHEQUE",
+        "balance": 2323232,
+        "id": "12345678919"
+      },
+      {
+        "type": "INVESTMENT",
+        "balance": 3333,
+        "id": "12345678920"
+      },
+      {
+        "type": "SAVING",
+        "balance": 77777,
+        "id": "12345678921"
+      },
+      {
+        "id": "0850",
+        "type": "",
+        "balance": 0
+      },
+      {
+        "id": "f842",
+        "type": "",
+        "balance": 0
+      },
+      {
+        "id": "4339",
+        "type": "",
+        "balance": 0
+      },
+      {
+        "id": "db02",
+        "type": "123",
+        "balance": "03213"
+      },
+      {
+        "id": "4e1f",
+        "type": "TFSA2",
+        "balance": "123213123"
+      },
+      {
+        "id": "fcc4",
+        "type": "SAVING",
+        "balance": 123
+      },
+      {
+        "id": "3572",
+        "type": "SAVING",
+        "balance": 12355
+      }
+    ]
+
+
+    const total = app.balanceSumUp(accounts)
+
+    expect(total).toEqual(126198812);
   });
+
+
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,74 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'cibc-app';
+  url = 'http://localhost:3000/accounts';
+
+
+  accounts: any = [];
+  accountType: string = '';
+  balance: number = 0;
+  accountTypeOptions = ['SAVING', 'CHEQUE', 'RRSP', 'RESP', 'TFSA', 'INVESTMENT', 'USSAVING', 'USCHEQUE']
+
+
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit() {
+    this.getAccounts();
+
+
+  }
+
+
+  getAccounts() {
+    this.http.get(this.url).subscribe((_data: any) => {
+      this.accounts = _data;
+      this.balanceSumUp(this.accounts);
+    })
+  }
+
+  sort() {
+    this.accounts.sort((a: any, b: any) => a.balance - b.balance)
+  }
+  addAccount(_type: string, _balance: number) {
+    let _payload = {
+      "type": _type,
+      "balance": _balance
+    }
+    this.http.post(this.url, _payload).subscribe(
+      _data => this.accounts.push(_data)
+    )
+  }
+
+  delete(_id: number) {
+    this.http.delete(`${this.url}/${_id}`).subscribe(
+      _data => this.getAccounts()
+    )
+  }
+
+  balanceSumUp(accounts: any): number | BigInt {
+    let total = 0;
+    accounts.forEach((e:any) => {
+      total = total + (Number(e.balance))
+    });
+    // total = accounts.reduce(
+    //   (sum: any, val: any) => sum + val?.balance, 0)
+
+    console.log(total);
+
+    return total ;
+
+  }
+
+
+
 }
